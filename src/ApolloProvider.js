140907fa55161f855f4306299e4d18169927aac4
Robert0B0 <70260478+Robert0B0@users.noreply.github.com>
2021-05-25 17:34:34 +0300

    Update ApolloProvider.js

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,8 +7,8 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-	/* uri: "https://social-lite-server.herokuapp.com/", */
-	uri: "http://localhost:5000/",
+	 uri: "https://social-lite-server.herokuapp.com/", 
+	 /*uri: "http://localhost:5000/", */
 });
 
 const authLink = setContext(() => {
